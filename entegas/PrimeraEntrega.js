6ab84ab481ef951625c584fc5749cb3e38f00282
tomas <tomasnavatta@gmail.com>
2023-11-09 23:17:50 -0300

    arreglo de errores

--- entegas/PrimeraEntrega.js
+++ entegas/PrimeraEntrega.js
@@ -1,64 +1,56 @@
-class ProductManager{
+class ProductManager {
+  constructor() {
+      this.product = [];
+      this.nextId = 1;
+  }
 
-    constructor(){
-        this.product = []
-        this.nextId = 1;
-    }
+  getProducts() {
+      return this.product;
+  }
 
-    getProduct(){
-       return this.product
-    }
-
-    AddProduct(title, description, price, thumbnail, code, stock){
-        if (!title || !price || !description || !thumbnail || !code || !stock) {
-            console.error('todos los datos son requeridos');
-            return;
-          }
-      
-          // Check if the code is unique
-          if (this.products.some(product => product.code === code)) {
-            console.error('el producto con este codigo ya existe');
-            return;
-          }
-
-        let id_product = (this.getProduct()).length
-        
-        const product = {
-            id: this.nextId++,
-            title: title,
-            description: description, 
-            price: price,
-            thumbnail: thumbnail,
-            code: code,
-            stock: stock
-        }
-        this.producto.push(product)
-        return this.producto
-    }
-
-    getProductById(id) {
-        const products = this.product.find(product => product.id === id);
-        if (!products) {
-          console.error('Product not found');
+  addProduct(title, description, price, thumbnail, code, stock) {
+      if (!title || !price || !description || !thumbnail || !code || !stock) {
+          console.error('Todos los datos son requeridos');
           return;
-        }
-        return products;
       }
-    
+
+      // Check if the code is unique
+      if (this.product.some(product => product.code === code)) {
+          console.error('El producto con este código ya existe');
+          return;
+      }
+
+      const product = {
+          id: this.nextId++,
+          title: title,
+          description: description,
+          price: price,
+          thumbnail: thumbnail,
+          code: code,
+          stock: stock
+      };
+
+      this.product.push(product);
+      return this.product;
+  }
+
+  getProductById(id) {
+      const product = this.product.find(product => product.id === id);
+      if (!product) {
+          console.error('Producto no encontrado');
+          return;
+      }
+      return product;
+  }
 }
 
-const productoManager = new ProductManager()
+const productoManager = new ProductManager();
 
-productoManager.AddProduct('Iphone', 'Pantalla de 6.7", 1 tb de alamacenamiento', 2000, 'https://toppng.com/uploads/preview/apple-iphone-15-pro-max-natural-titanium-png-11695041051y3hjkvpdvc.png', '15 pro max', 10);
-productoManager.AddProduct('Tv', 'Pantalla de 75", Tecnologia Oled', 10000, 'https://www.lg.com/mx/images/televisores/md07557421/gallery/D-01.jpg', 'LG UHD AI ThinQ 75 UQ80', 4);
+productoManager.addProduct('Iphone', 'Pantalla de 6.7", 1 tb de almacenamiento', 2000, 'https://toppng.com/uploads/preview/apple-iphone-15-pro-max-natural-titanium-png-11695041051y3hjkvpdvc.png', '15 pro max', 10);
+productoManager.addProduct('Tv', 'Pantalla de 75", Tecnología OLED', 10000, 'https://www.lg.com/mx/images/televisores/md07557421/gallery/D-01.jpg', 'LG UHD AI ThinQ 75 UQ80', 4);
 
-console.log(productoManager.getProducts());    
-
-
+console.log(productoManager.getProducts());
 console.log(productoManager.getProductById(1));
 console.log(productoManager.getProductById(2));
 console.log(productoManager.getProductById(3));
-  
-   
-  
- 
+
